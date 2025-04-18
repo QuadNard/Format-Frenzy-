775dbd4107998f38779befe71f28f43b4cf1b0e0
Justin Tulloch <101229476+QuadNard@users.noreply.github.com>
2025-04-18 19:31:18 +0000

    Initial setup

---
+++ frenzy-app/src/App.tsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <div className="min-h-screen flex items-center justify-center bg-white text-black text-2xl">
+      Quiz App is live ✅
+    </div>
+    </>
+  )
+}
+
+export default App
